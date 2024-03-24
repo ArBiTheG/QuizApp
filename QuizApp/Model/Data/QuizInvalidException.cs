828e0632a7ba16dcd8828c348671204c6dcd1d6c
Daniil <don.grabovsky@yandex.ru>
2024-03-24 22:22:02 +0300

    Update QuizInvalidException.cs

--- QuizApp/Model/Data/QuizInvalidException.cs
+++ QuizApp/Model/Data/QuizInvalidException.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,20 @@ namespace QuizApp.Model.Data
 {
     internal class QuizInvalidException: Exception
     {
-        public QuizInvalidException() { }
-        public QuizInvalidException(string message) : base(message) { }
+        public QuizInvalidException()
+        {
+        }
+
+        public QuizInvalidException(string message) : base(message)
+        {
+        }
+
+        public QuizInvalidException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected QuizInvalidException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
     }
 }
